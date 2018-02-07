b1584cafbaf5a5ea0c59ab8c69b69d9a9bb0ac5a
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-06 17:45:01 -0800

    Biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,25 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+int z;
+int x;
+int biggest; 
+
   cout<<"Please enter a whole number:\n";
-  
+ cin>> z;
   cout<<"Please enter another whole number:\n";
+ cin>> x;
+ if ( z >= x ) 
+ { biggest =z;
+}
+ else 
+ { biggest =x;
+ }
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
-
-  return 0;
+ 
+  cout<<"Of those two numbers, the biggest is:" << biggest<< endl;
+ cout<<endl<<"Thank you for playing.\n";
+ 
+ 
+ return 0;
 }
